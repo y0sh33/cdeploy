e0d3a0e38ae2230d91a3d0cb4219f18ef3057f30
Bryan <bryan+github@davidsons.name>
2015-02-16 16:11:19 -0500

    Bumping version to 1.1

--- setup.py
+++ setup.py
@@ -2,13 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name='cdeploy',
-    version='1.0',
+    version='1.1',
     description='A tool for managing Cassandra schema migrations',
     author='David McHoull',
     author_email='dmchoull@gmail.com',
     license='http://www.apache.org/licenses/LICENSE-2.0',
-    url='https://github.com/dmchoull/cdeploy',
-    download_url='https://github.com/dmchoull/cdeploy/archive/v1.0.tar.gz',
+    url='https://github.com/rackerlabs/cdeploy',
     keywords=['cassandra', 'migrations'],
     packages=find_packages(),
     install_requires=['PyYAML', 'cassandra-driver'],
