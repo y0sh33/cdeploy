1dc2d5b064a0975ce4a9f87cdf043b43299fb6f5
Kiyoshi Shikuma <kiyoshi@shopkick.com>
2016-08-03 11:43:39 -0700

    Bumping version to SK

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cdeploy',
-    version='1.6',
+    version='1.6-SK',
     description='A tool for managing Cassandra schema migrations',
     author='David McHoull',
     author_email='dmchoull@gmail.com',
